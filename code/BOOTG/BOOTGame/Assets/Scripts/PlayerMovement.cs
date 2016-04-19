fd2027893a59805b765f500fdede398fc6501e10
Douk vd Putten <dputten@hotmail.com>
2016-04-19 11:44:34 +0200

    Unity test person
    test person toegevoegd met een test text object. De text test object is
    90% zeker niet op de manier zoals het moet

--- code/BOOTG/BOOTGame/Assets/Scripts/PlayerMovement.cs
+++ code/BOOTG/BOOTGame/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     // Use this for initialization
     private Rigidbody2D rb;
+    public GameObject TestText;
     public float speed = 1.5f;
     public float maxspeed = 4.0f;
 
@@ -21,31 +22,52 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        // movement
+        // Movement
+
+        // We store the GetAxis Input which is premade by unity in MoveHorizontal and Vertical
+        // As we dont move vertical moveVertical becomes irrelevant
         float moveHorizontal = Input.GetAxis("Horizontal");
         //float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector3(moveHorizontal, 0.0f);
+        //in movement we store a new vector made of moveHorizontal and for vertical 0.0f as we dont move vertical
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
 
+        //movement gets multipited by speed. Because this happenes evry frame we scale te movement up.
         movement = movement * speed;
 
+        //Movement Max
         if (movement.x > maxspeed)
         {
             movement.x = maxspeed;
         }
 
+        //This is were we actually apply the movement onto the object.
         rb.AddForce(movement);
-
-        //collision
-        
         
     }
+
+    //Collision
     void OnCollisionEnter2D(Collision2D col)
     {
+        //if this object collides with object Door, Scene2 will be loaded.
         if(col.gameObject.name == "Door")
         {
             SceneManager.LoadScene("scene2");
         }
+
+        //if this object collides with object TestPerson, the method with the name "move" will be executed.
+        if(col.gameObject.name == "TestPerson")
+        {
+            TestText.SendMessage("Move");
+        }
     }
 
+    void OnCollisionExit2D(Collision2D col)
+    {
+        //if the object stops colliding with objec called TestPerson, the method with the name "remove" will be executed.
+        if (col.gameObject.name == "TestPerson")
+        {
+            TestText.SendMessage("ReMove");
+        }
+    }
 }
