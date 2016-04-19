465819dce84eaa6359504af39754981312a5fbac
Douk vd Putten <dputten@hotmail.com>
2016-04-19 15:01:04 +0200

    textbox v0.1
    Textbox onder de game, dialogue kan doorgevoerd worden door text files.
    textbox is nog niet geformat

---
+++ code/BOOTG/BOOTGame/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+        }
+
+        if(currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+	}
+}
