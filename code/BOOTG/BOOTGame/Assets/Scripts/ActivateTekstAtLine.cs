4a9d3a39bdb5325dba49fd620117f9bc7a20631f
Douk vd Putten <dputten@hotmail.com>
2016-04-26 15:27:14 +0200

    commenting
    alle code dialoge code is nu gecomment en iets logischer neergezet

--- code/BOOTG/BOOTGame/Assets/Scripts/ActivateTekstAtLine.cs
+++ code/BOOTG/BOOTGame/Assets/Scripts/ActivateTekstAtLine.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class ActivateTekstAtLine : MonoBehaviour {
 
-
+    // textbestand en textbox object
     public TextAsset theText;
+    public TextBoxManager textBox;
 
+    // int voor startline en endline
     public int startLine;
     public int endLine;
 
-    public TextBoxManager textBox;
-
-    public bool destroyWhenActivated;
-
-    public bool requireButtonPress;
-    private bool waitForPress;
+    // booleans om de behaviour van dit script te handelen.
+    public bool destroyWhenActivated; //destroy het object waar dit compenent in zit waarneer dit true is.
+    public bool requireButtonPress; // of er een keypress nodig is om de text te laten verschijnen
+    private bool waitForPress; // een bool om de keypress af te wachten
 
 	// Use this for initialization
 	void Start ()
     {
-
+        // de textbox word ingeladen d.m.v findobjectoftype zodat we geen textbox hoeven in te laden per NPC of trigger.
         textBox = FindObjectOfType<TextBoxManager>();
         	
 	}
@@ -27,8 +27,12 @@ public class ActivateTekstAtLine : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        // hier word pas text doorgevoerd zodra er een key input is.
+        // later moet er nog mouse input voor worden toegevoegd.
         if(waitForPress && Input.GetKeyDown(KeyCode.J))
         {
+            // de text word ingeladen in de textbox manager
+            // de startline en endline worden meegegeven om de tekst te handelen.
             textBox.ReloadScript(theText);
             textBox.currentLine = startLine;
             textBox.endAtLine = endLine;
@@ -40,16 +44,18 @@ public class ActivateTekstAtLine : MonoBehaviour {
             }
         }
 	}
-
+    // zodra er collision is
     void OnTriggerEnter2D(Collider2D coll)
     {
         if(coll.name == "Player")
         {
+            // en er een keybutton nodig is word waitforpress op true gezet
             if(requireButtonPress)
             {
                 waitForPress = true;
                 return;
             }
+            // anders word er op collision deze code uitgevoerd
             textBox.ReloadScript(theText);
             textBox.currentLine = startLine;
             textBox.endAtLine = endLine;
@@ -61,6 +67,7 @@ public class ActivateTekstAtLine : MonoBehaviour {
             }
         }
     }
+    // als er geen collision is kan de textbox niet geactivated worden d.m.v keypress.
     void OnTriggerExit2D(Collider2D coll)
     {
         if (coll.name == "Player")
--- code/BOOTG/BOOTGame/Assets/Scripts/TextBoxManager.cs
+++ code/BOOTG/BOOTGame/Assets/Scripts/TextBoxManager.cs
@@ -4,20 +4,19 @@ using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour {
 
-
+    //player object, tekst objects en textfiles
     public PlayerMovement player;
     public GameObject textBox;
-
     public Text theText;
-
     public TextAsset textFile;
+    //string array voor regels tekst
     public string[] textLines;
-
+    //ints om de lines bij te houden
     public int currentLine;
     public int endAtLine;
-
+    // een bool om de canvas aan en uit te zetten
     public bool isActive;
-
+    // een bool om de player te laten stoppen
     public bool stopPlayerMovement;
 
 
@@ -25,16 +24,17 @@ public class TextBoxManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        //als de tekst niet leeg is dan word de tekst opgesplits per enter
 	    if (textFile != null)
         {
             textLines = (textFile.text.Split('\n'));
         }
-
+        //als er geen endline word meegegeven word de enldline de max aantal regels
         if (endAtLine == 0)
         {
             endAtLine = textLines.Length - 1;
         }
-
+        // waarneer active word enable tekstbox aangeroepen, anders disable tekxtbox
         if (isActive)
         {
             EnableTekstBox();
@@ -48,24 +48,25 @@ public class TextBoxManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        //als het object niet active is word de code niet gerunned
         if (!isActive)
         {
             return;
         }
-
+        //de text in de canvas is de currentline
         theText.text = textLines[currentLine];
-
+        //zodra er enter word gedrukt word de volgende line ingeladen
         if (Input.GetKeyDown(KeyCode.Return))
         {
             currentLine += 1;
         }
-
+        //als de line bij endline aankomt word de textbox uitgezet
         if(currentLine > endAtLine)
         {
             DisableTekstBox();
         }
 	}
-
+    //een functie om de textbox aan te zetten en de player movement still te zetten. de playermovement is toggle baar
     public void EnableTekstBox()
     {
         textBox.SetActive(true);
@@ -75,14 +76,15 @@ public class TextBoxManager : MonoBehaviour {
             player.canMove = false;
         }
     }
-
+    // een funity om de textbox uit te zetten.
     public void DisableTekstBox()
     {
         textBox.SetActive(false);
         player.canMove = true;
         isActive = false;
     }
-
+    // in deze funtie word er een nieuw textbestand ingeladen. bijna hetzelfde aan het begin van het script
+    // maar dan bruikbaar voor activetekstlines.
     public void ReloadScript(TextAsset theText)
     {
         if (theText != null)
