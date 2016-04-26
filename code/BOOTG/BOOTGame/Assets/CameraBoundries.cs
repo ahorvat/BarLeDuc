5a3f7a8c1564b466f061ee850244499fe336590e
Juanalbergen <Juan.Albergen@hva.nl>
2016-04-26 15:31:25 +0200

    Add camera borders
    Needs improvement in centring the camera to the player after hitting a
    border.

---
+++ code/BOOTG/BOOTGame/Assets/CameraBoundries.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBoundries : MonoBehaviour
+{
+    private float minPosition = -11.20f;
+    private float maxPosition = 118f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < minPosition)
+        {
+            transform.position = new Vector3(minPosition, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > maxPosition)
+        {
+            transform.position = new Vector3(maxPosition, transform.position.y, transform.position.z);
+
+        }
+    }
+}
