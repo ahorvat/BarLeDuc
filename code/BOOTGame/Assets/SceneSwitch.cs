2168d1659482ef76b215d4ef8900c8c5d7345516
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-05-17 15:56:23 +0200

    added choices menu(THANKS DOUK)
    keuzemenu toegevoegd, scene switching naar de juiste scene na de
    gemeentebrief opties.

---
+++ code/BOOTGame/Assets/SceneSwitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+	public void sc_brief_home_google()
+    {
+        SceneManager.LoadScene("sc_brief_home_google");
+        return;
+    }
+
+    public void sc_brief_home_phone()
+    {
+        SceneManager.LoadScene("sc_brief_home_phonecall");
+        return;
+    }
+    public void sc_brief_home_buurvrouw()
+    {
+        SceneManager.LoadScene("sc_brief_home");
+        return;
+    }
+}
