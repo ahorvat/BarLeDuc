6c523eadbd6dc2106d04e6f9dbb3abfff3f07d29
Juanalbergen <Juan.Albergen@hva.nl>
2016-05-24 14:40:05 +0200

    Phonecall scene is fully working(again)

---
+++ code/BOOTGame/Assets/TBM_Extend.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TBM_Extend : TextBoxManager {
+    public GameObject DialogYoussef;
+    public GameObject DialogHenk;
+    public Scene activeScene;
+	// Use this for initialization
+	void Start () {
+        Scene activeScene = SceneManager.GetActiveScene();
+        DialogHenk.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        theText.text = textLines[currentLine];
+        if (textBox.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
+            {
+                currentLine += 1;
+            }
+        }
+
+        if (currentLine == 2)
+        {
+            DialogYoussef.SetActive(false);
+            DialogHenk.SetActive(true);
+        }
+        if (currentLine == 3)
+        {
+            DialogYoussef.SetActive(true);
+            DialogHenk.SetActive(false);
+        }
+        if (currentLine == 4)
+        {
+            DialogYoussef.SetActive(false);
+            DialogHenk.SetActive(true);
+        }
+        if (currentLine == 5)
+        {
+            DialogYoussef.SetActive(true);
+            DialogHenk.SetActive(false);
+        }
+        if (currentLine == 6)
+        {
+            SceneManager.LoadScene("sc_incorrectPhonecall");
+        }
+    }
+}
