16709d0e62c16387b9e67829ea2b8c96108311bc
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-05-26 01:04:00 +0200

    BOUNCING HARD YO

---
+++ code/BOOTGame/Assets/Scripts/bouncingTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncingTest : MonoBehaviour {
+    public float xPos, zPos;
+
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(xPos, Mathf.PingPong(Time.time, .5f) - 3, zPos);
+    }
+}
