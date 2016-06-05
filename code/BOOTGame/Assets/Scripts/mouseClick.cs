df0b712bae085d211c6ded8e05880c05294902fd
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-06-05 13:48:54 +0200

    added scene switching between BSN zoek scene & digidStap

--- code/BOOTGame/Assets/Scripts/mouseClick.cs
+++ code/BOOTGame/Assets/Scripts/mouseClick.cs
@@ -3,7 +3,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class mouseClick : MonoBehaviour {
+    private GameObject dialogueManager;
+    private DialogeManager dialogueScript;
 
+    void Start()
+    {
+        dialogueManager = GameObject.FindGameObjectWithTag("dialogueManager");
+        dialogueScript = dialogueManager.GetComponent<DialogeManager>();
+    }
 
     //use the OnMouseDown method of Unity to check whether the mouseposition is on the object and the mouse is clicked while its on the object.
     void OnMouseUp() {
@@ -20,6 +27,14 @@ public class mouseClick : MonoBehaviour {
         else if (gameObject.tag == "staatsloterijLetter")
         {
             SceneManager.LoadScene("sc_staatsloterijBrief_closeup");
+        }
+        else if (gameObject.tag == "pas")
+        {
+            if (dialogueScript.currentLine == dialogueScript.endAtLine)
+            {
+                SceneManager.LoadScene("sc_DigiDStap2");
+            }
+           
         }
 	}
     
