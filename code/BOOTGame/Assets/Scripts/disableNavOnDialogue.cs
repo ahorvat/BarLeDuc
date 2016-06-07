bcb9687c9f714bcbbcf88f9248beeb536900392e
Aleksej Horvat <ahorvat@gmail.com>
2016-06-07 23:35:14 +0200

    Add Disable Nav Script
    Navigation Should be disabled when Dialogue is active or Youssef is not in the scene

---
+++ code/BOOTGame/Assets/Scripts/disableNavOnDialogue.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disableNavOnDialogue : MonoBehaviour {
+
+	private PlayerMovement player;
+	private GameObject[] navArrows;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerMovement>();
+		navArrows = GameObject.FindGameObjectsWithTag("navArrow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!player || !player.canMove)
+		{
+			foreach (GameObject arrow in navArrows)
+			{
+				arrow.SetActive(false);
+			}
+		}
+	}
+}
