0ab176ab915d82d5f230a62abd4898960dbd6d9d
Douk vd Putten <dputten@hotmail.com>
2016-06-07 15:43:34 +0200

    Phonecall refined
    in de extension word de player sprite veranderd zodat de gebruiker kan
    zien dat er gebeld word
    tekst script aangepast
    klein script toegevoegd die dit mogelijk maakt

---
+++ code/BOOTGame/Assets/PhonecallChangePlayerSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhonecallChangePlayerSprite : MonoBehaviour {
+
+    private DialogeManager DM;
+    private SpriteRenderer playerSprite;
+    public Sprite spr_boy_calling;
+
+	// Use this for initialization
+	void Start () {
+        DM = FindObjectOfType<DialogeManager>();
+        playerSprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(DM.currentLine == 1)
+        {
+            playerSprite.sprite = spr_boy_calling;
+        }
+	
+	}
+}
