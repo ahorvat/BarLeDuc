ea7a52d867c78a064034394918710f9bff8f7462
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-06-08 14:58:08 +0200

    Fixed scaling script

--- code/BOOTGame/Assets/Scripts/Scaling.cs
+++ code/BOOTGame/Assets/Scripts/Scaling.cs
@@ -41,7 +41,7 @@ public class Scaling : MonoBehaviour
                 yield break;
             }   
         }
-
+        Debug.Log("JAJAJAJAJA");
         //Set scaling on true so the scaling starts.
         isScaling = true;
 
