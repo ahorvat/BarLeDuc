c3f32b7904e6f8d64bc87ee36d425d1d15b3c484
Douk vd Putten <dputten@hotmail.com>
2016-06-08 20:25:36 +0200

    sc_email_google + dialoge switcher

---
+++ code/BOOTGame/Assets/ActivateTBMatLastLine.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateTBMatLastLine : MonoBehaviour {
+
+    private TextBoxManager TBM;
+    private DialogeManager DM;
+
+	// Use this for initialization
+	void Start () {
+        TBM = FindObjectOfType<TextBoxManager>();
+        DM = FindObjectOfType<DialogeManager>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (DM.currentLine == DM.endAtLine)
+        {
+            TBM.EnableTekstBox();
+        }
+	
+	}
+}
