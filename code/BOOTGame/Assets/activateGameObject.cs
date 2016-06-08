57e42eab32f34197821f61bad31e6b09d4245cbc
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-06-08 20:59:13 +0200

    sc_buurvrouw_home disable arrow until dialog done fix

---
+++ code/BOOTGame/Assets/activateGameObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activateGameObject : MonoBehaviour {
+
+    private DialogeManager dialogueManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogueManager = GameObject.FindGameObjectWithTag("dialogueManager").GetComponent<DialogeManager>();
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (dialogueManager.currentLine == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
