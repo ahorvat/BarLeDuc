1a21ab0303b7e72b1a49908a40dcbd01d4551dea
Juanalbergen <Juan.Albergen@hva.nl>
2016-06-08 17:36:26 +0200

    Opzet About Boot

---
+++ code/BOOTGame/Assets/aboutBoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aboutBoot : MonoBehaviour {
+    public GameObject CanvasAbout;
+ 
+	// Use this for initialization
+	void Start () {
+        CanvasAbout.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void isPressed()
+    {
+        CanvasAbout.SetActive(true);
+    }
+    
+    public void gaVerder()
+    {
+        CanvasAbout.SetActive(false);
+    }
+}
